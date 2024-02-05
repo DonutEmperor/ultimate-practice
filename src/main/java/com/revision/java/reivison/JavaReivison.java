921cf7bd43705355aa80d553f1633d8b057d074c
maxsonjoget <maxson.chen@joget.com>
2024-02-06 02:50:59 +0800

    Upload local project to remote repo

---
+++ src/main/java/com/revision/java/reivison/JavaReivison.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.revision.java.reivison;
+
+/**
+ *
+ * @author Maxson
+ */
+public class JavaReivison {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
